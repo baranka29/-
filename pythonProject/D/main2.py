89507ff77de0b4fcf1654b5e3573a07f6369cd88
baranka29 <126053149+baranka29@users.noreply.github.com>
2023-03-15 11:37:31 +0300

    Add files via upload

---
+++ pythonProject/D/main2.py
@@ -0,0 +1,16 @@
+import requests
+
+host = 'http://127.0.0.1:5000'
+
+r = requests.post(host,
+                  json={'data': [1, 2, 3]})
+print(r.text)
+r = requests.post(host,
+                  json={'data': [1, 2, 3]})
+
+assert r.json() == {'sum': 6}
+
+r = requests.post(host,
+                  json={'data': [-1, -2, -3]})
+
+assert r.json() == {'sum': -6}
